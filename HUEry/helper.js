e9adda7b51e9ad8682678d3c699ffc454000847f
PseudoHUEry <harry-kevin@hotmail.com>
2021-08-03 13:52:10 -0300

    feature: Instalação puppeteer e require, inclusão de comando _page e sua function.

--- HUEry/helper.js
+++ HUEry/helper.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js'),
 client = new Discord.Client(),
 config = require('./config.json'),
 texts = require('./texts/texts.json'),
+pteer = require('puppeteer'),
 
 
 helper = [
@@ -13,15 +14,44 @@ helper = [
          msg.channel.send(`Fale uma frase com: ${texts.significado[Math.floor(Math.random() * texts.significado.length)].toUpperCase()}.`)
             }
         },
-    // Utilidades
+   
+        _page = {
+            functionName: '!page',
+            call: `${config.prefix}`,
+            descricao:'',
+            comand: function (msg, arr) {
+
+            console.log(`${this.functionName} está rodando.`)
+                let scrap = async() => {
+                    const browser = await pteer.launch({
+                        headless: false
+                    })
+                
+                const page = await browser.newPage()
+                await page.goto(arr)
+                await page.screenshot({
+                    path: 'example.png'})
+                browser.close()
+                    return[]
+            }
+        }
+        },
+
+        // Utilidades
     
-    _help = {
+        _help = {
         functionName: 'help',
         call: `${config.prefix}`,
         descricao:'',
         comand: function (msg) {
             msg.author.send(`Commands; \n ${this._game.functionName.toUpperCase()}: ${this._game.descricao} \n ${this._help.functionName.toUpperCase()}: ${this._help.descricao}`)
         }  
+    },
+    
+        _error = {
+        comand: function (msg) {
+            msg.channel.send(`Error: Verifique vosso comando.`)
+        } 
     },   
 ]
 
--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // // Server Web
-// const _ex = require("express"),
-//   app = _ex(),
-//   port = 3000,
-//   home = require("./homepage/routerHomepage.js");
+const _ex = require("express"),
+  app = _ex(),
+  port = 3000,
+  home = require("./homepage/routerHomepage.js");
 
-//   app.use(port, (req, res) => {
-//     res.send("<h1> Bem vindo! </h1>")
-//     client.on (helper[2].comand('', 'Na página web')    
-// )});
+  app.use(port, (req, res) => {
+    res.send("<h1> Bem vindo! </h1>")
+    client.on (helper[2].comand('', 'Na página web')    
+)});
 
 // DISCORD
 const Discord = require("discord.js"),
@@ -19,16 +19,16 @@ client.once("ready", () => console.log("Servidor Discord - online"));
 
 client.login(config.BOT_TOKEN);
 
-client.on("message", message => {
+client.on("msg", msg => {
   let arr = message.content.split(" ");
 
     helper.forEach(x => {
     const {call, functionName, comand} = x
-     if (`${call}${functionName}` == arr[0]){
-        comand(message)
-        }
-    });
-});
+      if (`${call}${functionName}` == arr[0]){
+        comand(msg)
+      }
+    })
+  });
 
 
 // Integrate
